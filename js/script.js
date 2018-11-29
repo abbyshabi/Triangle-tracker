91ad2561068e7f019defa4209cd31f2f74bae955
abby-shabi <abbyshabi@gmail.com>
2018-11-29 23:06:25 +0300

    updated my HTML form

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ function getTriangleType() {
 	var c = parseInt(document.getElementById("cValue").value);
 
 	if(a == b && b == c) {
-		document.getElementById('answer').innerHTML = 'Equilateral'
+		document.getElementById('answer').value = 'Equilateral'
 	}
 	else if (a == b || a == c || b == c){
-		document.getElementById('answer').innerHTML = 'Isoceles'
+		document.getElementById('answer').value = 'Isoceles'
 	}
 	else if (a != b != c) {
 		//console.log(a+b)
 		if (((a + b) <= c) || ((a + c) <= b) || ((c + b) <= a)) {
-		document.getElementById('answer').innerHTML = 'Not a Triangle'
+		document.getElementById('answer').value = 'Not a Triangle'
 	} 
 	else {
-		document.getElementById('answer').innerHTML = 'Scalene'
+		document.getElementById('answer').value = 'Scalene'
 	}
 }
 	
