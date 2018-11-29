b93dd843592f57a860126820740d577e4cc96b66
abby-shabi <abbyshabi@gmail.com>
2018-11-29 22:32:39 +0300

    declared the function and variables in the js file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function getTriangleType() {
+
+	var a = parseInt(document.getElementById("aValue").value);
+	var b = parseInt(document.getElementById("bValue").value);
+	var c = parseInt(document.getElementById("cValue").value);
