f1b722a875bfb5a158aa266091e08be778ae37ed
abby-shabi <abbyshabi@gmail.com>
2018-11-29 22:33:50 +0300

    added the if statement for equilateral

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ function getTriangleType() {
 	var a = parseInt(document.getElementById("aValue").value);
 	var b = parseInt(document.getElementById("bValue").value);
 	var c = parseInt(document.getElementById("cValue").value);
+
+	if(a == b && b == c) {
+		document.getElementById('answer').innerHTML = 'Equilateral'
+	}
