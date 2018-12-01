ed2728f40bdba209db88f69ce683824576d0b98d
abby-shabi <abbyshabi@gmail.com>
2018-12-01 21:29:40 +0300

    added styles to my css

--- js/script.js
+++ js/script.js
@@ -5,20 +5,20 @@ function getTriangleType() {
 	X[0] = parseInt(document.getElementById("aValue").value);
 	X[1] = parseInt(document.getElementById("bValue").value);
 	X[2] = parseInt(document.getElementById("cValue").value);
-	
-	if(a == b && b == c) {
-		document.getElementById('answer').value = 'Equilateral'
+
+	if(X[0] == X[1]  && X[1]  == X[2]) {
+		document.getElementById('answer').value = 'An Equilateral: An equilateral triangle is a triangle with three equal sides'
 	}
-	else if (a == b || a == c || b == c){
-		document.getElementById('answer').value = 'Isoceles'
+	else if (X[0] == X[1]  || X[0] == X[2]|| X[1]  == X[2]){
+		document.getElementById('answer').value = 'An Isoceles : An Isoceles triangle is a triangle with two equal sides and the third side is different'
 	}
-	else if (a != b != c) {
+	else if (X[0] != X[1]  != X[2]) {
 		//console.log(a+b)
-		if (((a + b) <= c) || ((a + c) <= b) || ((c + b) <= a)) {
+		if (((X[0] + X[1] ) <= X[2]) || ((X[0] + X[2]) <= X[1] ) || ((X[2] + X[1] ) <= X[0])) {
 		document.getElementById('answer').value = 'Not a Triangle'
 	} 
 	else {
-		document.getElementById('answer').value = 'Scalene'
+		document.getElementById('answer').value = 'A Scalene : A scalene triangle doesn\'t have any equal side, all the sides are different from one another'
 	}
 }
 	
